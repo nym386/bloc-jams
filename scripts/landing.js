fc3625b6a133a86afa185cf846bf220a0fe8e596
Josh Doying <josh.doying@gmail.com>
2017-07-03 11:48:23 -0400

    Assignment 9 Frontend Foundation complete: Create a replica of forEach function and apply in functions

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,19 +1,19 @@
+//storing the points in an array
 var pointsArray = document.getElementsByClassName('point');
 
+
+//function to animates the points
 var animatePoints = function(points){
+  //function to control the type of animation
   var revealPoint = function(index){
-      points[index].style.opacity = 1;
-      points[index].style.transform = "scaleX(1) translateY(0)";
-      points[index].style.msTransform = "scaleX(1) translateY(0)";
-      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+      index.style.opacity = 1;
+      index.style.transform = "scaleX(1) translateY(0)";
+      index.style.msTransform = "scaleX(1) translateY(0)";
+      index.style.WebkitTransform = "scaleX(1) translateY(0)";
     }
-
-    for (var i = 0; i < points.length; i ++){
-      revealPoint(i);
-    }
-      points[1].style.color = "yellow";
+    forEach(points, revealPoint);
+    points[1].style.color = "yellow";
 };
-
 
 window.onload = function(){
   if (window.innerHeight > 950){
