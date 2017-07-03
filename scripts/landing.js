77f35a774798509d7965af7439a13522131ad2e2
Josh Doying <josh.doying@gmail.com>
2017-07-03 09:14:00 -0400

    Checkpoint 9 Complete: Add event listener's to animation

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,31 @@
-var revealPoint = function(index){
-  var points = index;
-  for (var i = 0; i < index.length; i ++){
-    points[i].style.opacity = 1;
-    points[i].style.transform = "scaleX(1) translateY(0)";
-    points[i].style.msTransform = "scaleX(1) translateY(0)";
-    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points){
+  var revealPoint = function(index){
+      points[index].style.opacity = 1;
+      points[index].style.transform = "scaleX(1) translateY(0)";
+      points[index].style.msTransform = "scaleX(1) translateY(0)";
+      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+
+    for (var i = 0; i < points.length; i ++){
+      revealPoint(i);
+    }
+      points[1].style.color = "yellow";
+};
+
+
+window.onload = function(){
+  if (window.innerHeight > 950){
+    animatePoints(pointsArray);
   }
-    points[1].style.color = "yellow";
-  };
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+      animatePoints(pointsArray);
+    }
+  });
+}
