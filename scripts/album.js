f04cd286bb36ec67db4f1521498618983629fe31
Josh Doying <josh.doying@gmail.com>
2017-07-05 14:48:02 -0400

    Checkpoint 12 Frontend Foundation Complete: Add Play Button animation to Album tables

--- scripts/album.js
+++ scripts/album.js
@@ -32,7 +32,7 @@ var albumMarconi = {
 var createSongRow = function(songNumber, songName, songLength){
   var template =
         '<tr class="album-view-song-item">'
-      + '<td class="song-item-number">' + songNumber +'</td>'
+      + '<td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber +'</td>'
       + '<td class="song-item-title">' + songName + '</td>'
       + '<td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -40,13 +40,18 @@ var createSongRow = function(songNumber, songName, songLength){
 
       return template;
 }
+// Album button templatevar
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
 
 var setCurrentAlbum = function(album) {
-  var albumTitle = document.getElementsByClassName('album-view-title')[0];
-  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-  var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-  var albumImage = document.getElementsByClassName('album-cover-art')[0];
-  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 
   albumTitle.firstChild.nodeValue = album.title;
   albumArtist.firstChild.nodeValue = album.artist;
@@ -62,4 +67,17 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function(){
   setCurrentAlbum(albumPicasso);
-} ;
+
+  albumSongList.addEventListener('mouseover', function (event){
+    //this is the only section I'm a little confused about. Isn't every parent === album-view-song-item?
+    if (event.target.parentElement.className === 'album-view-song-item'){
+      event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+    }
+  });
+
+  for (var i = 0; i < songRows.length; i++) {
+    songRows[i].addEventListener('mouseleave', function(event){
+      this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+    });
+  }
+}
